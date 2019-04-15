3797647b32eeab32c879d6c1a10e964428ebf380
hem bhatt <hembhatttku@gmail.com>
2019-04-15 10:50:46 +0530

    new

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+        <span>Copyright © kumaun university nainital | Online Affiliation</span>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-light justify-content-center" >
+    <a class="navbar-brand" href="#">
+      <img src="{{asset('images/name.png')}}"  class="img-fluid" >      
+    </a>
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #42f4c5;">
+        <a class="navbar-brand" href="#">Online Affiliation Portal</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav nav ml-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>     
+            
+          </ul>
+          
+        </div>
+      </nav>
+
